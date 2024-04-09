7118bf0af0bd1210be654530d54bc946134d0ca7
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-04-09 01:02:25 -0400

    finished login page:
    - added animation to background text
    - added description to login card

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './LoginPage.scss';
+
+const SplitText = ({ target }) => {
+  useEffect(() => {
+    function splitTextIntoSpans(target) {
+      let elements = document.querySelectorAll(target);
+      elements.forEach((element) => {
+        element.classList.add('split-text');
+        let text = element.innerText;
+        let splitText = text
+          .split(" ")
+          .map(function (word) {
+            let char = word.split('').map(char => {
+              return `<span class="split-char">${char}</span>`;
+            }).join('');
+            return `<div class="split-word">${char}&nbsp;</div>`;
+          }).join('');
+
+        element.innerHTML = splitText;
+      });
+    }
+
+    splitTextIntoSpans(target);
+  }, [target]);
+
+  return null; // This component doesn't render anything visible
+};
 
 const LoginPage = () => {
   const handleLogin = (event) => {
     console.log('clicked login')
     event.preventDefault(); 
-    
-
   };
 
   return (
     <div className='background'>
-      <h1 className="title-text">Remembrance</h1>
+      <div className="bubble-text title-text">Remembrance</div>
+      <SplitText target=".bubble-text" /> {/* Include the SplitText component */}
       <div className="login-container">
-        <h2>Please Sign In</h2>
+        <h2>remembrance.</h2>
+        <p><b>remembrance</b> is a tool designed to help you correlate an uploaded photo with photos you have previously uploaded onto Google photos. 
+        This website aims to create nostalgia and let you appreciate the past. 
+        </p>
         <div className="google-sign-in-button" onClick={handleLogin}>
-        <img
-          src="https://developers.google.com/static/identity/images/branding_guideline_sample_dk_rd_lg.svg"
-          alt="Sign in with Google"
-        />
-      </div>
+          <img
+            src="https://developers.google.com/static/identity/images/branding_guideline_sample_dk_rd_lg.svg"
+            alt="Sign in with Google"
+          />
+        </div>
       </div>
     </div>
   );
